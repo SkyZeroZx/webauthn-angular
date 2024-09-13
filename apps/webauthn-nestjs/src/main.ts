c67f1c2fa75efe3f702bf9c659dbf4a6429fbdff
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2024-09-12 23:00:42 -0500

    feat(webauthn-nestjs): Set global configs

--- apps/webauthn-nestjs/src/main.ts
+++ apps/webauthn-nestjs/src/main.ts
@@ -1,19 +1,44 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import cors from 'cors';
+import helmet from 'helmet';
 
-import { Logger } from '@nestjs/common';
+import { loggerConfig } from '@core/config';
+import { ResponseInterceptor } from '@core/interceptors';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, { ...loggerConfig });
+
 	const globalPrefix = 'api';
+
 	app.setGlobalPrefix(globalPrefix);
+
 	const port = process.env.PORT || 3000;
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true
+		})
+	);
+
+	app.use(
+		cors({
+			methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+			credentials: true
+		})
+	);
+
+	app.useGlobalInterceptors(new ResponseInterceptor());
+
+	app.use(helmet());
+
+	app.use(helmet.hidePoweredBy());
+
 	await app.listen(port);
+
 	Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
